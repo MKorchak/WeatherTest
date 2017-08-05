100fbd6e56003aa7f03efe48d78862998346c436
Misha Korchak <mishakorchak@MacBook-Pro-Misha.local>
2017-08-05 18:40:04 +0300

    first commit

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Misha Korchak on 05.08.17.
+//  Copyright © 2017 Misha Korchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
